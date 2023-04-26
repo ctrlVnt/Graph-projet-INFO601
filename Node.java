26fe54e2ab47a211adb4f1de28a50dc848f329f0
ctrlVnt <giordanobruno227@gmail.com>
2023-04-26 15:37:42 +0200

    ciao

--- Node.java
+++ Node.java
@@ -1,3 +1,48 @@
-public class Node {
-    
+public class Node<S> {
+
+  private S nodeName;
+
+  public Node(S nodeName) {
+    this.nodeName = nodeName;
+  }
+
+  public S getNodeName() {
+    return this.nodeName;
+  }
+
+
+  public void setNodeName(S nodeName) {
+    this.nodeName = nodeName;
+  }
+  
+  /**
+   * Overrides the method that calculates the hash value
+   * If two objects are equal according to the equals(Object) method,
+   * then calling the hashCode method on each of the two objects must produce the
+   * same integer result.
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 19;
+
+    result = prime * result + nodeName.hashCode();
+
+    return result;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public boolean equals(Object o) {
+    if (o == null) {
+      return false;
+    } else {
+      Node<S> tmp = (Node<S>) o;
+      if ((this.nodeName.equals(tmp.nodeName))) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 }
