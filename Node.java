95a748ba57499c399ccb269245eaaa9d670fe77a
Boruus <boris.trombert@gmail.com>
2023-04-26 13:36:10 +0200

    v1

--- Node.java
+++ Node.java
@@ -1,3 +1,30 @@
+import java.util.Objects;
+
 public class Node {
-    
+    private String label;
+
+    public Node(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Node)) {
+            return false;
+        }
+        Node node = (Node) obj;
+        return Objects.equals(label, node.getLabel());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
+    }
 }
